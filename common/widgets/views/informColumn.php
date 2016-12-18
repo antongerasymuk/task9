cb3318fdebef0dd408991898b4735a1faf7225f2
Anton <bron_off1@ukr.net>
2016-12-18 21:30:42 +0200

    add custom widget

---
+++ common/widgets/views/informColumn.php
@@ -0,0 +1,9 @@
+<div class="col s12 m4">
+	<div class="icon-block">
+		<h2 class="center light-blue-text"><i class="<?= $imageClass; ?>"></i></h2> 
+		<h5 class="center"><?= $title; ?></h5>
+		<p class="light">
+			<?= $content; ?>
+		</p>
+	</div>
+</div>
